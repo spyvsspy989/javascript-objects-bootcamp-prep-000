6948e47dd821f7638745560496d44b3153a87057
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-06 05:11:44 +0000

    automatically backed up by learn

--- test/objects-test.js
+++ test/objects-test.js
@@ -4,28 +4,87 @@ const expect = require('chai').expect
 const fs = require('fs')
 const jsdom = require('mocha-jsdom')
 const path = require('path')
-
-describe('objects', () => {
   jsdom({
     src: fs.readFileSync(path.resolve(__dirname, '..', 'objects.js'), 'utf-8')
   })
-
-  it('defines an object called `playlist`', () => {
-    expect(typeof playlist).to.equal('object')
-    expect(Object.keys(playlist).length).to.be.greaterThan(0)
+  ('defines an object called `playlist`', () => {
+    expect(typeof playlist).to.equal('Object')
+    expect(Object.keys(playlist).length).to.be.greaterThan()
   })
-
-  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
-    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
-      expect(updatePlaylist({}, 'Phil Ochs', "Here's to the State of Mississippi")).
+var playlist = {}
+var playlist = new Object;
+var playlist = {Object: "playlistArtistName"};
+var playlist = new playlistArtistName({'playlistartistName': "Phil 0chs"});
+var playlist = playlistartistName, playlistsongTitle;
+({1: "Heres to the State of Mississippi"});
+var playlistArtistName = 'artistName';
+var playlistArtistName2 = 'artistName2'
+var playlistsongTitle = 'songTitle'
+var playlistsongTitle2 = 'songTitle2'
+var playlistsongTitle = 'songTitle';
+function updateObjectWithKeyAndValue(obj, key, value) {
+Object.assign({}, playlist, { ['artistName']: 'songTitle' })
+}
+function updateObjectWithObject(playlist, artistName, songTitle){
+  return Object.assign({}, {['artistName']: 'Phil 0chs, Welcome to Mississippi'})
+}
+  describe('updateObjectWithKeyAndValue(playlist, artistName, songTitle)', () => {
+    ('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      expect(updateObjectWithKeyAndValue({}, 'artistName', 'songTitle')).
         to.eql({ 'Phil Ochs': "Here's to the State of Mississippi" })
     })
   })
+  return Object [playlist2 - playlistArtistName2]}
+  var playlistArtistName2 = 'artistName2'
+  var playlistsongTitle2 = 'songTitle2'
+  var playlistArtistName2 = "Kanye";
+  var songTitle2 = 'Gold Digger';
+  /*global describe, it */
 
-  describe('removeFromPlaylist(playlist, artistName)', () => {
-    it('removes `artistName` from `playlist`', () => {
-      expect(removeFromPlaylist({ Kanye: "Gold Digger" }, "Kanye")).
-        to.eql({})
+const expect = require('chai').expect
+const fs = require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+  jsdom({
+    src: fs.readFileSync(path.resolve(__dirname, '..', 'objects.js'), 'utf-8')
+  })
+  it('defines an object called `playlist`', () => {
+    expect(typeof playlist).to.equal('Object')
+    expect(Object.keys(playlist).length).to.be.greaterThan()
+  })
+var playlist = {}
+var playlist = new Object;
+var playlist = {Object: "playlistArtistName"};
+var playlist = new playlistArtistName({'playlistartistName': "Phil 0chs"});
+var playlist = playlistartistName, playlistsongTitle;
+({'Phil 0chs': "Heres to the State of Mississippi"});
+var playlistArtistName = 'artistName';
+var playlistArtistName2 = 'Kanye'
+var playlistsongTitle = 'songTitle'
+var playlistsongTitle2 = 'Gold Digger'
+var playlistsongTitle = 'songTitle';
+function updateObjectWithKeyAndValue(obj, key, value) {
+Object.assign({}, playlist, { ['artistName']: 'songTitle' })
+}
+function updateObjectWithObject(playlist, artistName, songTitle){
+  return Object.assign({}, {['artistName']: 'Phil 0chs, Welcome to Mississippi'})
+}
+  describe('updateObjectWithKeyAndValue(playlist, artistName, songTitle)', () => {
+    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      expect(updateObjectWithKeyAndValue({}, 'Phil Ochs', "Here's to the State of Mississippi")).
+        to.eql({ 'Phil Ochs': "Here's to the State of Mississippi" })
     })
   })
-})
+  var playlistArtistName2 = "Kanye";
+  var songTitle2 = 'Gold Digger';
+  var playlistArtistName2 = 'artistName2'
+  var playlistsongTitle2 = 'songTitle2'
+  function removeFromPlaylist(playlist2, playlistArtistName2) {
+  return Object [playlist2 - playlistArtistName2]}
+  describe('removeFromPlaylist(playlist2, playlistlistArtistName2)', () => {
+    it('removes `playlistArtistName2` from `playlist2`', () => }
+      expect(removeFromPlaylist({ Kanye: "Gold Digger", "Kanye")).
+        to.eql({})}
+};
+
+  
